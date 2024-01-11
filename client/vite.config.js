113a5919ee0ac978db22177763b030b57a895784
Nidhinsanju <sanjuronaldo77@gmail.com>
2024-01-11 21:50:46 +0530

    change in dependancy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    ReactRefresh(),
     react(),
     {
       name: "vite-plugin-vtt",
