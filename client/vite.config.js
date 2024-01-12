1ffb07249943fa9c1641752bc957d4180c572d74
Nidhinsanju <sanjuronaldo77@gmail.com>
2024-01-12 15:48:09 +0530

    Changes to ESM

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ReactRefresh } from "vite-plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    ReactRefresh(),
     react(),
-
     {
       name: "vite-plugin-vtt",
       enforce: "pre",
